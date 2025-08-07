c53b244569065a71c0cf3a8133cf8d882c6a6de2
Elizabeth Wang <chezelizabethwang@gmail.com>
2025-08-07 01:23:41 -0700

    add definitions.h template file, update gitignore

---
+++ definitions.h
@@ -0,0 +1,8 @@
+// definitions.h
+// This file is simply a template. Once filled out, it contains sensitive information! Do not share with others once personal information has been defined.
+#pragma once
+
+// Telegram BOT Token (Get from Botfather)
+#define BOT_TOKEN "INSERT BOT TOKEN HERE"
+// Chat ID to send the message to (get this from Telegram by messaging the bot and checking updates)
+#define CHAT_ID "INSERT CHAT ID HERE"
